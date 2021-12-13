be1fc98e0543cdcb32414e11c4a597565c0320b8
nguyenlamvu123 <nguyenlamvu123@gmail.com>
2021-12-13 17:58:10 +0700

    13122021

---
+++ nhap.py
@@ -0,0 +1,15 @@
+alltitle = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+#car.update({"color": "White"})
+#car = dict.fromkeys(alltitle)
+
+z = list(alltitle.items())[:2]
+##print(z[1][1])
+print(dict(z))
+
+##alltitle.update(dict.fromkeys(['samgiongzon', 'hahahehehoho']))
+##print(alltitle)
